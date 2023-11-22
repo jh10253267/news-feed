594ca3198d2d640c259386795b43b5fa4b5d8851
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-22 17:52:54 +0900

    #7-멤버 엔티티에 한줄 코멘트필드 추가 및 수정을 위한 메소드 생성
    요구사항에 맞게 한줄 코멘트 필드를 추가했고 수정을 위한 메소드인 changeXXX메소드 작성

--- src/main/java/com/sparta/domain/Member.java
+++ src/main/java/com/sparta/domain/Member.java
@@ -2,14 +2,23 @@ package com.sparta.domain;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@Setter
+@Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member extends BaseEntity{
     @Id
     private String username;
     private String password;
+    private String content;
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+    public void changeContent(String content) {
+        this.content = content;
+    }
 }
