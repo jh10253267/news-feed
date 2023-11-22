cb569b96527636779b53ed7b0d3ed7401d7dd036
yeongdo99 <atpsfka1@gmail.com>
2023-11-22 15:10:55 +0900

    #6-댓글 레포지토리 작성

--- src/main/java/com/sparta/repository/CommentRepository.java
+++ src/main/java/com/sparta/repository/CommentRepository.java
@@ -4,5 +4,5 @@ import com.sparta.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    //
 }
