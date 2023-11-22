c87851c327ba7819201299aecbd820aaf8325375
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-22 15:26:40 +0900

    #5-전체 게시글 목록 조회를 위한 쿼리메소드 추가
    전체 게시글 리스트를 조회하기 위한 쿼리메소드를 레포지토리에 추가했다.

--- src/main/java/com/sparta/repository/BoardRepository.java
+++ src/main/java/com/sparta/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.sparta.repository;
 import com.sparta.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByOrderByIdDesc();
 }
