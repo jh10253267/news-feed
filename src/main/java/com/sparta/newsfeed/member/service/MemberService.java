63b986c025f8dada0a55e3ab4eaba1b2e0fb4916
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-11-23 21:57:33 +0900

    #feat 34 멤버 서비스 내부에서 세터를 사용하는 부분 수정

--- src/main/java/com/sparta/newsfeed/member/service/MemberService.java
+++ src/main/java/com/sparta/newsfeed/member/service/MemberService.java
@@ -4,7 +4,9 @@ import com.sparta.newsfeed.member.domain.Member;
 import com.sparta.newsfeed.member.dto.RequestProfileUpdateDto;
 import com.sparta.newsfeed.member.dto.SignupDto;
 import com.sparta.newsfeed.member.repository.MemberRepository;
+import com.sparta.newsfeed.security.service.MemberDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -38,33 +40,33 @@ public class MemberService {
     }
 
     @Transactional
-    public Member updateMember(RequestUpdateProfileDto updateProfileDto, MemberDetailsImpl memberDetails){
+    public void updateMember(RequestProfileUpdateDto updateProfileDto, MemberDetailsImpl memberDetails){
         String loginUsername = memberDetails.getUsername();
         String updateProfileUsername = updateProfileDto.getUsername();
 
         if(!loginUsername.equals(updateProfileUsername)){
             throw new IllegalArgumentException("해당 로그인 유저네임과 변경하려는 사용자의 유저네임이 다릅니다.");
         }
+
         Member memberEntity = memberRepository.findById(updateProfileUsername).orElseThrow(()-> new IllegalArgumentException("해당 아이디는 찾을 수 없습니다."));
 
         String rawPassword = updateProfileDto.getPassword();
         // 첫번째 입력받은 비밀번호
         String rawPasswordConfirm = updateProfileDto.getPasswordConfirm();
         // 두 번째 입력받은 비밀번호
-        if (StringUtils.isNotBlank(rawPassword) || StringUtils.isNotBlank(rawPasswordConfirm)) {
+        if (StringUtils.isNotBlank(rawPassword) || StringUtils.isNotBlank(rawPasswordConfirm))  {
             // 비밀번호 일치 확인
             if (!rawPassword.equals(rawPasswordConfirm)) {
                 throw new IllegalArgumentException("첫 번째 비밀번호와 확인 비밀번호가 일치하지 않습니다.");
             }
-
-            // 새 비밀번호를 사용하여 비밀번호 업데이트
-            String bcrytPassword = bCryptPasswordEncoder.encode(rawPassword);
-            memberEntity.setPassword(bcrytPassword);
         }
+        // 새 비밀번호를 사용하여 비밀번호 업데이트
+        String bcrytPassword = passwordEncoder.encode(rawPassword);
 
-        memberEntity.setUsername(updateProfileUsername);
-        memberEntity.setContent(updateProfileDto.getContent());
+        memberEntity.changePassword(bcrytPassword);
+        memberEntity.changeContent(updateProfileDto.getContent());
+        // change함수 활용해서 password, content를 memberEntity에 저장
 
-        return memberEntity;
+        memberRepository.save(memberEntity);
     }
 }
