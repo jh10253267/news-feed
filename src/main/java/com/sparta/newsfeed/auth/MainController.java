b2e44e85d8bd8789a347c63b4e73339b777a0c99
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-11-25 14:14:17 +0900

    #50 회원 가입 구현

--- src/main/java/com/sparta/newsfeed/auth/MainController.java
+++ src/main/java/com/sparta/newsfeed/auth/MainController.java
@@ -1,14 +1,36 @@
 package com.sparta.newsfeed.auth;
 
+import com.sparta.newsfeed.member.dto.SignupDto;
+import com.sparta.newsfeed.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class MainController {
 
+    private final MemberService memberService;
+
     @GetMapping("/user/login-page")
     public String loginPage() {
-        System.out.println("-loginPage-");
         return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage(){
+        return "signup";
     }
 }
