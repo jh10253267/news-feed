147b02f1d8c30fa7ca741193d4c34204f6642630
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-26 05:11:44 +0900

    #25-rest controller advice작성
    발생하는 예외를 처리하기 위한 customRestAdvice작성

---
+++ src/main/java/com/sparta/newsfeed/advice/CustomRestAdvice.java
@@ -0,0 +1,59 @@
+package com.sparta.newsfeed.advice;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+@Log4j2
+public class CustomRestAdvice {
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleBindException(BindException e) {
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        if (e.hasErrors()) {
+            BindingResult bindingResult = e.getBindingResult();
+
+            bindingResult.getFieldErrors().forEach(fieldError -> {
+                errorMap.put(fieldError.getField(), fieldError.getCode());
+            });
+
+        }
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+    @ExceptionHandler({NoSuchElementException.class, EmptyResultDataAccessException.class})
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e) {
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("message", "No Such Element Exception");
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.FOUND)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(Exception e) {
+
+        log.error(e);
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("massage", e.getMessage());
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+
+
+}
