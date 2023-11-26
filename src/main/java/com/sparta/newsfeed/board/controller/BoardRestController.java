f037c7428674d57cadd2ac353cbfac0fa5944417
Gwon JunHyeok <jh10253267@gmail.com>
2023-11-27 01:23:49 +0900

    #62-이미지 업로드를 위해 보드 컨트롤러 수정

--- src/main/java/com/sparta/newsfeed/board/controller/BoardRestController.java
+++ src/main/java/com/sparta/newsfeed/board/controller/BoardRestController.java
@@ -1,5 +1,6 @@
 package com.sparta.newsfeed.board.controller;
 
+import com.sparta.newsfeed.board.dto.BoardListAllDTO;
 import com.sparta.newsfeed.board.dto.BoardRequestDTO;
 import com.sparta.newsfeed.board.dto.BoardResponseDTO;
 import com.sparta.newsfeed.board.service.BoardService;
@@ -16,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -29,9 +31,9 @@ public class BoardRestController {
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> register(@Valid @RequestBody BoardRequestDTO boardRequestDTO,
                                                         BindingResult bindingResult,
-                                                        @AuthenticationPrincipal MemberDetailsImpl memberDetails) throws BindException{
+                                                        @AuthenticationPrincipal MemberDetailsImpl memberDetails) throws BindException {
         log.info(bindingResult.hasErrors());
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             throw new BindException(bindingResult);
         }
         Map<String, Object> map = new HashMap<>();
@@ -39,33 +41,45 @@ public class BoardRestController {
         map.put("result", "success");
         return ResponseEntity.ok(map);
     }
+
+    @Operation(summary = "모든 게시글 보기", description = "GET방식으로 모든 게시물과 포함된 이미지를 보여준다.")
+    @GetMapping("/")
+    public ResponseEntity<Map<String, Object>> readAll() {
+        Map<String, Object> map = new HashMap<>();
+        List<BoardListAllDTO> boardListAllDTOList = boardService.listWithAll();
+        map.put("boards", boardListAllDTOList);
+        return ResponseEntity.ok(map);
+    }
+
     @Operation(summary = "게시글 조회", description = "게시글 고유 번호를 통해 게시글을 조회할 수 있다.")
     @GetMapping("/{id}")
     public ResponseEntity<Map<String, Object>> read(@PathVariable("id") Long id) {
         Map<String, Object> map = new HashMap<>();
         BoardResponseDTO boardResponseDTO = boardService.read(id);
-        map.put("board",boardResponseDTO);
+        map.put("board", boardResponseDTO);
         return ResponseEntity.ok(map);
     }
+
     @Operation(summary = "게시글 수정", description = "로그인한 사용자는 자신의 게시물을 수정할 수 있다.")
     @PutMapping("/{id}")
     public ResponseEntity<Map<String, Object>> modify(@PathVariable("id") Long id, @Valid @RequestBody BoardRequestDTO boardRequestDTO,
-                                      BindingResult bindingResult,
-                                      @AuthenticationPrincipal MemberDetailsImpl memberDetails) throws BindException {
-        if(bindingResult.hasErrors()) {
+                                                      BindingResult bindingResult,
+                                                      @AuthenticationPrincipal MemberDetailsImpl memberDetails) throws BindException {
+        if (bindingResult.hasErrors()) {
             throw new BindException(bindingResult);
         }
         Map<String, Object> map = new HashMap<>();
 
         boardService.modify(id, boardRequestDTO, memberDetails.getMember());
 
-        map.put("result","success");
+        map.put("result", "success");
         return ResponseEntity.ok(map);
     }
+
     @Operation(summary = "게시글 삭제", description = "로그인한 사용자는 자신의 게시물을 삭제할 수 있다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> delete(@PathVariable("id") Long id,
-                                      @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
+                                                      @AuthenticationPrincipal MemberDetailsImpl memberDetails) {
         boardService.delete(id, memberDetails.getMember());
         Map<String, Object> map = new HashMap<>();
         map.put("result", "success");
