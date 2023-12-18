b34a6b268f53b995c232c33472da7611682bae70
Gwon JunHyeok <jh10253267@gmail.com>
2023-12-18 16:14:34 +0900

    #80-역할에 맞게 클래스 이름 수정
    MainController보다 MainMvcController가 더 적절한 것 같아서 수정

--- src/main/java/com/sparta/newsfeed/auth/MainMvcController.java
+++ src/main/java/com/sparta/newsfeed/auth/MainMvcController.java
@@ -1,26 +1,15 @@
 package com.sparta.newsfeed.auth;
 
-import com.sparta.newsfeed.member.dto.SignupDto;
 import com.sparta.newsfeed.member.service.MemberService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
-public class MainController {
+public class MainMvcController {
 
     private final MemberService memberService;
 
